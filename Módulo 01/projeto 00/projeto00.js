3e7a85786913c7a01b6b9bd1874bf2e7f205f974
magalhaesbruno <magalhaesbrunomb@gmail.com>
2022-05-28 20:50:24 -0300

    finalizando alterações

--- Módulo 01/projeto 00/projeto00.js
+++ Módulo 01/projeto 00/projeto00.js
@@ -1,21 +1,48 @@
 const prompt = require("prompt-sync")();
 console.clear();
-let qnt = 0
-let q = ''
-for (i = 0; i < 5; i++){
-  q = prompt('Digite SIM OU NÃO:').toLocaleLowerCase();
-  if (q == 'sim'){
-    qnt +=1
-  }
+console.log(
+  `Olá, houve um assassinato na cozinha, e estamos investigando o caso`
+);
+console.log("Por favor, responda apenas com SIM OU NÃO.");
+let respostas = [];
+q1 = prompt("Você estava na piscina às 23:10h? ").toLocaleLowerCase();
+if (q1 == "sim") {
+  respostas.push("sim");
 }
-switch (qnt){
-  case 4:
-    console.log('Você é culpado.')
+q2 = prompt("Você fuma? ").toLocaleLowerCase();
+if (q2 == "sim") {
+  respostas.push("sim");
+}
+q3 = prompt(`conhece a residência? `).toLocaleLowerCase();
+if (q3 == "sim") {
+  respostas.push("sim");
+}
+q4 = prompt(`Você é destro? `).toLocaleLowerCase();
+if (q4 == "sim") {
+  respostas.push("sim");
+}
+q5 = prompt(`Você sabe quem foi a vitíma? `).toLocaleLowerCase();
+if (q5 == "sim") {
+  respostas.push("sim");
+}
+switch (respostas.length) {
+  case 0:
+    console.log(`Você é inocente, está liberado.`);
     break;
-  case 3:
-    console.log('Você é suspeito.')
+
+  case 1:
+    console.log(`Você é inocente, está liberado.`);
     break;
   case 2:
-    console.log('Você é inocente..')
+    console.log(`Você é inocente, está liberado.`);
+    break;
+  case 3:
+    console.log(`Você a partir de agora é um suspeito, não saia da cidade.`);
+    break;
+  case 4:
+    console.log(`Você é o culpado, está preso`);
+    break;
+  case 5:
+    console.log(`Você é o culpado, está preso`);
     break;
 }
--- Módulo 01/projeto 01/projeto01.js
+++ Módulo 01/projeto 01/projeto01.js
@@ -8,42 +8,58 @@ passado e com isso em poucos dias o mundo irá entrar em colpaso e o mesmo irá 
 console.log("Por favor, responda apenas com SIM OU NÃO.");
 let respostas = [];
 q1 = prompt("Você sabe configurar um canhão de prótons?").toLocaleLowerCase();
-if (q1 == 'sim'){
-  respostas.push('sim')
+if (q1 == "sim") {
+  respostas.push("sim");
 }
-q2 = prompt("Você sabe configurar um canhão de prótons?").toLocaleLowerCase();
-if (q2 == 'sim'){
-  respostas.push('sim')
+q2 = prompt("Você sabe utilizar um canhão de prótons?").toLocaleLowerCase();
+if (q2 == "sim") {
+  respostas.push("sim");
 }
-q3 = prompt(`Você tem conhecimento para  consertar os motores de uma nave?`).toLocaleLowerCase();
-if (q3 == 'sim'){
-  respostas.push('sim')
+q3 = prompt(
+  `Você tem conhecimento para  consertar os motores de uma nave?`
+).toLocaleLowerCase();
+if (q3 == "sim") {
+  respostas.push("sim");
 }
-q4 = prompt(`Possui conhecimento sobre viagens interdimensionais?`).toLocaleLowerCase();
-if (q4 == 'sim'){
-  respostas.push('sim')
+q4 = prompt(
+  `Possui conhecimento sobre viagens interdimensionais?`
+).toLocaleLowerCase();
+if (q4 == "sim") {
+  respostas.push("sim");
 }
-q5 = prompt(`Você está pronto para se sacrificar em prol da humanidade?`).toLocaleLowerCase();
-if (q5 == 'sim'){
-  respostas.push('sim')
+q5 = prompt(
+  `Você está pronto para se sacrificar em prol da humanidade?`
+).toLocaleLowerCase();
+if (q5 == "sim") {
+  respostas.push("sim");
 }
-switch (respostas.length){
+switch (respostas.length) {
   case 0:
-    console.log(`Você falhou miseravelmente ${personagem}, agora viva com a destruição do mundo em suas mãos.`)
+    console.log(
+      `Você falhou miseravelmente ${personagem}, agora viva com a destruição do mundo em suas mãos.`
+    );
     break;
   case 1:
-    console.log(`Você é um covarde ${personagem}, fugir não adianta nada, iremos morrer assim.`)
+    console.log(
+      `Você é um covarde ${personagem}, fugir não adianta nada, iremos morrer assim.`
+    );
     break;
   case 2:
-    console.log(`Você é um covarde ${personagem}, fugir não adianta nada, iremos morrer assim.`)
+    console.log(
+      `Você é um covarde ${personagem}, fugir não adianta nada, iremos morrer assim.`
+    );
     break;
   case 3:
-    console.log(`Após diversas tentativas você quase conseguiu ${personagem}, mas não foi o suciente, estamos condenados a morte.`)  
+    console.log(
+      `Após diversas tentativas você quase conseguiu ${personagem}, mas não foi o suciente, estamos condenados a morte.`
+    );
     break;
   case 4:
-    console.log(`Você conseguiu ${personagem}, porém, custou sua vida.`)
+    console.log(`Você conseguiu ${personagem}, porém, custou sua vida.`);
     break;
   case 5:
-    console.log(`Parabéns ${personagem}, você destruiu o meteoro facilmente, talvez você deva se tornar um viajante inter-dimensional.`)
+    console.log(
+      `Parabéns ${personagem}, você destruiu o meteoro facilmente, talvez você deva se tornar um viajante inter-dimensional.`
+    );
     break;
 }
